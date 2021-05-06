34a599df1215978a4a8b50eed887ddb51cbb264e
Brian D. Young <83609467+s3m4nt@users.noreply.github.com>
2021-05-05 20:16:10 -0700

    Add files via upload
    "Wednesday night upload"

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,9 +5,20 @@
 	For example, "Hello, my name is Anthony. I'm 20 years old and live in 100 Main St."
 	Log this variable to the console (console.log(yourVariableName)). 
 	Check the console to make sure you're getting the desired result.
-
 */
 
+// Create 4 vars name, age, address, greeting
+const nme = 'Joe Smith';
+const age = 25;
+const address = '1234 Main St.';
+const greeting =
+  'Hello, my name is ' +
+  nme +
+  ". I'm " +
+  age +
+  ' years old and live at ' +
+  address;
+console.log(greeting);
 
 /*
 	Part 2: Create another variable called greeting2. This time, use the
@@ -17,9 +28,10 @@
 
 	Log this variable to the console (console.log(yourVariableName)). 
 	Check the console to make sure you're getting the desired result.
-
 */
 
+const greeting2 = `Hello, my name is ${nme}. I'm ${age} years old and live at ${address}`;
+console.log(greeting2);
 
 /* 
 	Part 3: "Code Testing." Let's check and see if we did everything correctly and that `greeting` returns the same value as `greeting2`.
@@ -27,6 +39,11 @@
 	If the return is false, correct your bugs above until `greeting` and `greeting2` are equivalent. 
 */
 
+// let problemSolved = true;
+// console.log(problemSolved);
+
+let problemSolved = greeting === greeting2;
+console.log(problemSolved);
 
 /*
 	Part 4: (No coding is needed for this part).
@@ -42,3 +59,8 @@
 	Bonus: What would the pseudocode look like if you also had to find the smallest number?
 */
 
+// Pseudo Code for Part 4
+// I would use Math.max() or Math.min()
+const hiNum = Math.max(23, 222, 645, 3, 78, 99, 123);
+const hiNumStr = `The highest number in the hiNum const is ${hiNum}.`;
+console.log(hiNumStr);
